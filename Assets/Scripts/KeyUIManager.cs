c8728e1a42a54dd1ec3ccdb98057b166a6c1677a
Piturnah <peterhebden6@gmail.com>
2021-10-03 19:05:23 +0100

    We wobble

--- Assets/Scripts/KeyUIManager.cs
+++ Assets/Scripts/KeyUIManager.cs
@@ -14,13 +14,13 @@ public class KeyUIManager : MonoBehaviour
 
         // TODO: Make generic for any gap size
         float startingOffset = -60 * gc.keysToPress.Count + 60;
-        Debug.Log(startingOffset);
         for (int i = 0; i < gc.keysToPress.Count; i++) {
             float newPosX = startingOffset + 120 * i;
             GameObject newImageObject = Instantiate(keyImage, Vector3.zero, Quaternion.identity, transform);
             newImageObject.GetComponent<RectTransform>().anchoredPosition = Vector3.right * newPosX;
-            newImageObject.GetComponent<KeyButton>().UpdateKeyValue(gc.keysToPress[i]);
-
+            KeyButton newImageKeyButtonComp = newImageObject.GetComponent<KeyButton>();
+            newImageKeyButtonComp.UpdateKeyValue(gc.keysToPress[i]);
+            newImageKeyButtonComp.gc = gc;
         }
     }
 
