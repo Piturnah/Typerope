0cadc84bea42a22ea2ebdf3f68152f9e827e6b56
Alex Noles <alexnoles@outlook.com>
2021-10-04 18:23:36 +0100

    Slight animation change

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int currentScore;
+    private void Update()
+    {
+        
+    }
+}
